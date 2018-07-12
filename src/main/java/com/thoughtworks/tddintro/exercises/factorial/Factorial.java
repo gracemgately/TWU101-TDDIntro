263811a3341183d55448d2a700dd551c077b2f72
Grace Gately <gracemgately@gmail.com>
2018-07-12 15:27:55 -0400

    all factorial tests passing

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,15 +2,21 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 0){
+        if (i < 0){
+            throw new IllegalArgumentException();
+        }
+        else if (i == 0){
             return 1;
         }
         else if (i <= 2){
             return i;
         }
         else {
-            Integer nextFact = i * compute(i - 1) * compute(i - 2);
-            return nextFact;
+            Integer factorial = i;
+            for (Integer j = i - 1; j > 0; j--){
+                factorial *= j;
+            }
+            return factorial;
         }
     }
 }
