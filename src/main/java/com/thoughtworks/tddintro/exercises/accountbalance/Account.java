8bf29d7fcfda1e281c7a4db2a672bb0b1b9a25b6
Grace Gately <gracemgately@gmail.com>
2018-07-12 15:37:11 -0400

    added: shouldIncreaseBalance test passing

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,9 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    public Integer balance = 0;
+
+    public void deposit(Integer amount){
+        balance += amount;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,11 +3,17 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
         Account account = new Account();
+        assertThat(account.balance, is(0));
+
+        account.deposit(50);
+        assertThat(account.balance, is(50));
     }
 
     @Test
