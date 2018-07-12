1a7e540c255719a8b85d2bcb3ecc5c324ad6a544
Grace Gately <gracemgately@gmail.com>
2018-07-12 15:23:23 -0400

    added: factorial exercises to shouldReturnSixWhenNumberIsThree

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,16 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if (i == 0){
+            return 1;
+        }
+        else if (i <= 2){
+            return i;
+        }
+        else {
+            Integer nextFact = i * compute(i - 1) * compute(i - 2);
+            return nextFact;
+        }
     }
 }
+
